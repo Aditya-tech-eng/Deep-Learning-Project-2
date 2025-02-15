0651161b2d8dffa1d2d13d42e4e3387381751fb3
Aditya-tech-eng <adityadeshmukh012001@gmail.com>
2025-02-15 13:10:18 +0530

    Update home.js

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -216,7 +216,7 @@ export const ImageUpload = () => {
             Aditya Pratap Deshmukh : Potato Disease Classification
           </Typography>
           <div className={classes.grow} />
-          <Avatar src={cblogo}></Avatar>
+         
         </Toolbar>
       </AppBar>
       <Container maxWidth={false} className={classes.mainContainer} disableGutters={true}>
